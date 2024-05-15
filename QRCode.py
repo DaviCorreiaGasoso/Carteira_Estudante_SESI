f9cc9c7eec8e2d8e5f55120e91fe69015c2f94cd
EmillyMarrocos <emillymarrocos5@gmail.com>
2024-05-15 15:35:57 -0300

    QR Code do Aluno

--- QRCode.py
+++ QRCode.py
@@ -3,6 +3,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from conexao_bd import connect
 import mysql.connector
 import qrcode
 
