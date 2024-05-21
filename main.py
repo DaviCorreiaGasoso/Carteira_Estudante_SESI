705674f717ff6c23b533da81032cde7c2d854c49
DaviCorreiaGasoso <davicorreiao07@gmail.com>
2024-05-21 15:36:40 -0300

    Tela atualizadas

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.app import MDApp
-from kivymd.uix.floatlayout import MDFloatLayout
 
+class Gerenciador (ScreenManager):
+    pass
 
-class LoginScreen(Screen):
+class Login_Estudantes(Screen):
+    pass
+
+class Tela_Estudantes(Screen):
+    pass
+
+class Login_Inicial(Screen):
+    pass
+
+class Login_Pais(Screen):
+    pass
+
+class Tela_Pais (Screen):
+    pass
+
+class Login_Funcionarios(Screen):
+    pass
+
+class Tela_Funcionarios(Screen):
     pass
 
 class LoginApp(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Blue"
-        
         return Builder.load_file('main.kv')
 
     def verificar_login(self):
